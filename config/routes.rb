1487be206fa4bf581c7de90a3bbc7884705cfc1d
Kevin Moormann <kevin.moormann@parivedasolutions.com>
2012-03-28 15:50:27 -0500

    updated home route matching

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  match '/', :to => 'pages#home'
 
   root :to => 'pages#home'
 
